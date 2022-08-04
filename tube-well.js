a1c275caa1f9e89522337b20ec8ed4bfafc7785e
Jack Hsu <90829467+xg6636@users.noreply.github.com>
2022-08-04 22:16:19 +0800

    🦄 refactor:

--- tube-well.js
+++ tube-well.js
@@ -1,22 +1,25 @@
 // 单井出水量
 // lib003
 // created at 2022/08/01 23:50:09
-// last modified at 2022/08/04 19:17:13
+// last modified at 2022/08/04 22:15:09
 // 
 // copyright (c) 2022 Jack Hsu
 
 
 
 var tubeWell = {
-    standards: ["JGJ120"],
-    JGJ120: {
-        specific: "JGJ120-2012",
-        getMaxCapacity: function (a) {
-            // param a: sample {filterRadius: 0.3, filterLength: 18, k: 18}
-            var aux = 120 * Math.PI;
-            aux = aux * Number(a.filterRadius) * Number(a.filterLength);
-            aux = aux * Math.pow(Number(a.k), 0.333333333)
-            return aux
-        }
+    getMaxCapacity: function (a) {
+        // param a: sample {filterRadius: 0.3, filterLength: 18, k: 18}
+        var out = 120 * Math.PI;
+        out = out * Number(a.filterRadius) * Number(a.filterLength);
+        out = out * Math.pow(Number(a.k), 0.333333333);
+        return { specification: "JGJ120-2012", value: out }
+    },
+    getFilterLength: function (a) {
+        // param a: sample {wellCapacity:1200, filterDiameter: 0.6, filterNe: 0.4, k: 18}
+        var v = Math.sqrt(Number(a.k)) / 15;
+        var out = Number(a.wellCapacity) / Math.PI;
+        out = out / Number(a.filterDiameter) / Number(a.filterNe) / v;
+        return { specification: "工程地质手册（第五版）", value: out }
     }
 }
