5184d89e69458878317748cc0e87da267c62eb32
Jack Hsu <90829467+xg6636@users.noreply.github.com>
2022-08-06 15:37:38 +0800

    🐞 fix:

--- evaluate-strut.js
+++ evaluate-strut.js
@@ -1,13 +1,13 @@
 // 支撑轴压比计算
 // lib002
 // created at 2022/08/01 14:39:51
-// last modified at 2022/08/06 12:58:11
+// last modified at 2022/08/06 15:37:20
 // 
 // copyright (c) 2022 Jack Hsu
 
 
 
-import { queryFc as queryConcreteFc } from "material-concrete.js";
+import { queryFc as queryConcreteFc } from "/material-concrete.js";
 
 const concreteStrut = {
     getAxialStrength: function (strut) {
