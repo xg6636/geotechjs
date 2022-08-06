446b98a6830075da711bc63a845caad38e78a555
Jack Hsu <90829467+xg6636@users.noreply.github.com>
2022-08-06 16:19:01 +0800

    🦄 refactor:

--- evaluate-strut.js
+++ evaluate-strut.js
@@ -1,7 +1,7 @@
 // 支撑轴压比计算
 // lib002
 // created at 2022/08/01 14:39:51
-// last modified at 2022/08/06 15:54:46
+// last modified at 2022/08/06 16:17:36
 // 
 // copyright (c) 2022 Jack Hsu
 
@@ -11,14 +11,14 @@ const concreteStrut = {
     getAxialStrength: function (strut) {
         // param strut: sample { width: "700", height: "800", concreteLevel: "C30" }
         var s = Number(strut.width) * Number(strut.height);
-        return concrete.GB50010.queryFc(strut.concreteLevel) * s * 0.001
+        return concrete.queryFc(strut.concreteLevel).value * s * 0.001
     },
     getAxialForce: function (site) {
         // param site: sample { pressure: 234, distance: 8.5, angle: 90, steps: 1, loadFactor: 1.35 }
         var f = Number(site.pressure); // 围压
-        f = f * Number(site.distance); // 支撑间距
-        f = f * Number(site.loadFactor); // 荷载系数
-        f = f * Number(site.steps); // 分担跨数
+        f *= Number(site.distance); // 支撑间距
+        f *= Number(site.loadFactor); // 荷载系数
+        f *= Number(site.steps); // 分担跨数
         var a = Number(site.angle); // 支撑角度
         f = f / Math.sin(a / 180 * Math.PI);
         return f
--- material-concrete.js
+++ material-concrete.js
@@ -1,53 +1,49 @@
 // 混凝土强度
 // lib001
 // created at 2022/08/01 14:39:51
-// last modified at 2022/08/06 15:54:58
+// last modified at 2022/08/06 16:15:52
 // 
 // copyright (c) 2022 Jack Hsu
 
 
 
 const concrete = {
-    standards: ["GB50010"],
-    GB50010: {
-        specific: "GB50010-2010",
-        queryFck: function (a) {
-            var fck = {
-                "C15": 10.0,
-                "C20": 13.4,
-                "C25": 16.7,
-                "C30": 20.1,
-                "C35": 23.4,
-                "C40": 26.8,
-                "C45": 29.6,
-                "C50": 32.4,
-                "C55": 35.5,
-                "C60": 38.5,
-                "C65": 41.5,
-                "C70": 44.5,
-                "C75": 47.4,
-                "C80": 50.2
-            }
-            return fck[String(a).toUpperCase()]
-        },
-        queryFc: function (a) {
-            var fc = {
-                "C15": 7.2,
-                "C20": 9.6,
-                "C25": 11.9,
-                "C30": 14.3,
-                "C35": 16.7,
-                "C40": 19.1,
-                "C45": 21.1,
-                "C50": 23.1,
-                "C55": 25.3,
-                "C60": 27.5,
-                "C65": 29.7,
-                "C70": 31.8,
-                "C75": 33.8,
-                "C80": 35.9
-            }
-            return fc[String(a).toUpperCase()]
+    queryFck: function (a) {
+        var fck = {
+            "C15": 10.0,
+            "C20": 13.4,
+            "C25": 16.7,
+            "C30": 20.1,
+            "C35": 23.4,
+            "C40": 26.8,
+            "C45": 29.6,
+            "C50": 32.4,
+            "C55": 35.5,
+            "C60": 38.5,
+            "C65": 41.5,
+            "C70": 44.5,
+            "C75": 47.4,
+            "C80": 50.2
         }
+        return { specific: "GB50010-2010", value: fck[String(a).toUpperCase()] }
+    },
+    queryFc: function (a) {
+        var fc = {
+            "C15": 7.2,
+            "C20": 9.6,
+            "C25": 11.9,
+            "C30": 14.3,
+            "C35": 16.7,
+            "C40": 19.1,
+            "C45": 21.1,
+            "C50": 23.1,
+            "C55": 25.3,
+            "C60": 27.5,
+            "C65": 29.7,
+            "C70": 31.8,
+            "C75": 33.8,
+            "C80": 35.9
+        }
+        return { specific: "GB50010-2010", value: fc[String(a).toUpperCase()] }
     }
 }
