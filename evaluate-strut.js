91e3e2dd8b65aa3b3d44dd93e10a454ec0ed6c13
Jack Hsu <90829467+xg6636@users.noreply.github.com>
2022-08-06 13:00:04 +0800

    🦄 refactor:

--- evaluate-strut.js
+++ evaluate-strut.js
@@ -1,17 +1,19 @@
 // 支撑轴压比计算
 // lib002
 // created at 2022/08/01 14:39:51
-// last modified at 2022/08/04 19:16:56
+// last modified at 2022/08/06 12:58:11
 // 
 // copyright (c) 2022 Jack Hsu
 
 
 
-var concreteStrut = {
+import { queryFc as queryConcreteFc } from "material-concrete.js";
+
+const concreteStrut = {
     getAxialStrength: function (strut) {
         // param strut: sample { width: "700", height: "800", concreteLevel: "C30" }
         var s = Number(strut.width) * Number(strut.height);
-        return concrete.GB50010.queryFc(strut.concreteLevel) * s * 0.001
+        return queryConcreteFc(strut.concreteLevel) * s * 0.001
     },
     getAxialForce: function (site) {
         // param site: sample { pressure: 234, distance: 8.5, angle: 90, steps: 1, loadFactor: 1.35 }
