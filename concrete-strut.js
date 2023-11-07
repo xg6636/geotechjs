e56d2a111d5b3a249014690d7bdba8e31ab238bd
Jack Hsu <90829467+xg6636@users.noreply.github.com>
2023-11-07 21:56:29 +0800

    Update and rename evaluate-strut.js to concrete-strut.js

--- concrete-strut.js
+++ concrete-strut.js
@@ -7,27 +7,26 @@
 
 
 
-const concreteStrut = {
-    getAxialStrength: function (strut) {
+const concrete_strut = {
+    get_axial_strength: function (strut) {
         // param strut: sample { width: "700", height: "800", concreteLevel: "C30" }
         var s = Number(strut.width) * Number(strut.height);
-        return concrete.queryFc(strut.concreteLevel).value * s * 0.001
+        return gb_concrete_material.query_fc(strut.concrete_level).value * s * 0.001;
     },
-    getAxialForce: function (site) {
+    get_axial_force: function (site) {
         // param site: sample { pressure: 234, distance: 8.5, angle: 90, steps: 1, loadFactor: 1.35 }
         var f = Number(site.pressure); // 围压
         f *= Number(site.distance); // 支撑间距
-        f *= Number(site.loadFactor); // 荷载系数
+        f *= Number(site.load_factor); // 荷载系数
         f *= Number(site.steps); // 分担跨数
         var a = Number(site.angle); // 支撑角度
         f = f / Math.sin(a / 180 * Math.PI);
-        return f
+        return f;
     },
     evaluate: function (strut, site) {
-        var f = this.getAxialForce(site);
-        var r = this.getAxialStrength(strut);
+        var f = this.get_axial_force(site);
+        var r = this.get_axial_strength(strut);
         var f_r = (f / r).toFixed(3);
-        return "轴力: " + (f).toFixed(3) + "kN\n" + "支撑强度: "
-            + (r).toFixed(3) + "kN\n" + "轴压比: " + f_r
+        return "轴力: " + (f).toFixed(3) + "kN\n" + "支撑强度: " + (r).toFixed(3) + "kN\n" + "轴压比: " + f_r;
     }
 }
