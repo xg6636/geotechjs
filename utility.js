bc26e8d18c7d93ba8f746e4bddd9b7e5ae388c6f
Jack Hsu <90829467+xg6636@users.noreply.github.com>
2023-11-07 22:05:09 +0800

    Update utility.js

--- utility.js
+++ utility.js
@@ -1,21 +1,41 @@
 // utilities
 // lib004
-// created at 2022/08/02 10:08:21
-// last modified at 2022/08/04 19:17:18
+// created at 2022/08/02 10:08:21f
+// last modified at 2023-11-07 22:00:35
 // 
 // copyright (c) 2022 Jack Hsu
 
 
 
-function createFormObjectByItemName(formName) {
-    // Caution: out’s property value is string!!!
-    var out = new Object();
-    var frm = document.forms[formName];
-    for (i = 0; i < frm.length; i++) {
-        out[frm[i].name] = frm[i].value;
+function get_form_data(form_id) {
+    var f = document.getElementById(form_id);
+    var out = {};
+    var e;
+    for (i = 0; i < f.length; i++) {
+        if (f[i].name != '' && f[i].disabled == false) {
+            e = f[i].name.replaceAll("-", "_");
+            switch (f[i].type.toLowerCase()) {
+                case 'checkbox':
+                    if (out[e] == undefined) {
+                        out[e] = [];
+                    }
+                    if (f[i].checked) {
+                        out[e].push(f[i].value);
+                    }
+                    break;
+                case 'radio':
+                    if (f[i].checked) {
+                        out[e] = f[i].value;
+                    }
+                    break;
+                default:
+                    out[e] = f[i].value;
+            }
+        }
     }
     return out;
 }
+
 
 function createFormObjectByItemId(formName) {
     // Caution: out’s property value is string!!!
@@ -26,3 +46,4 @@ function createFormObjectByItemId(formName) {
     }
     return out;
 }
+
