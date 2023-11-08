00c511dc92973071b6233603c15833240cb86856
Jack Hsu <90829467+xg6636@users.noreply.github.com>
2023-11-08 17:04:08 +0800

    Update concrete-material.js

--- concrete-material.js
+++ concrete-material.js
@@ -1,14 +1,14 @@
 // 混凝土强度
 // lib001
 // created at 2022/08/01 14:39:51
-// last modified at 2023-11-07 22:53:51
+// last modified at 2023-11-08 10:00:39
 // 
 // copyright (c) 2022 - 2023 Jack Hsu
 
 
 
-const gb_concrete_material = {
-    query_fck: function (a) {
+const concreteMaterialGB = {
+    queryFck: function (a) {
         let fck = {
             "C15": 10.0,
             "C20": 13.4,
@@ -27,7 +27,7 @@ const gb_concrete_material = {
         };
         return { specific: "GB50010-2010", value: fck[String(a).toUpperCase()] };
     },
-    query_fc: function (a) {
+    queryFc: function (a) {
         let fc = {
             "C15": 7.2,
             "C20": 9.6,
