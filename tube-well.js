6da5131a70404c2ca98ac2f673adcd04c8e8deab
Jack Hsu <90829467+xg6636@users.noreply.github.com>
2023-11-08 17:06:57 +0800

    Update tube-well.js

--- tube-well.js
+++ tube-well.js
@@ -1,25 +1,26 @@
 // 单井出水量
 // lib003
 // created at 2022/08/01 23:50:09
-// last modified at 2023-11-07 22:51:10
+// last modified at 17:06 2023/11/8
 // 
 // copyright (c) 2022 - 2023 Jack Hsu
 
 
 
-const tube_well = {
-    get_max_capacity: function (a) {
+const tubeWell = {
+	specific: ["JGJ120-2012","工程地质手册（第五版）"],
+    getMaxCapacity(a) {
         // param a: sample {filter_radius: 0.3, filter_length: 18, k: 18}
-        var out = 120 * Math.PI;
+        let out = 120 * Math.PI;
         out = out * Number(a.filter_radius) * Number(a.filter_length);
         out = out * Math.pow(Number(a.k), 0.333333333);
-        return { specific: "JGJ120-2012", value: out }
+        return { specific: "JGJ120-2012", value: out };
     },
-    get_filter_length: function (a) {
+    getFilterLength(a) {
         // param a: sample {well_capacity:1200, filter_diameter: 0.6, filter_ne: 0.4, k: 18}
-        var v = Math.sqrt(Number(a.k)) / 15;
-        var out = Number(a.well_capacity) / Math.PI;
+        let v = Math.sqrt(Number(a.k)) / 15;
+        let out = Number(a.well_capacity) / Math.PI;
         out = out / Number(a.filter_diameter) / Number(a.filter_ne) / v;
-        return { specific: "工程地质手册（第五版）", value: out }
+        return { specific: "工程地质手册（第五版）", value: out };
     }
 }
