f68b34c9c3b6fe1c9b0641a2b3339ef389c10ae4
Jack Hsu <90829467+xg6636@users.noreply.github.com>
2023-11-08 17:08:19 +0800

    Update utility.js

--- utility.js
+++ utility.js
@@ -1,17 +1,17 @@
 // utilities
 // lib004
 // created at 2022/08/02 10:08:21f
-// last modified at 2023-11-07 22:52:04
+// last modified at 17:07 2023/11/8
 // 
 // copyright (c) 2022 - 2023 Jack Hsu
 
 
 
-function get_form_data(form_id) {
-    var f = document.getElementById(form_id);
-    var out = {};
-    var e;
-    for (i = 0; i < f.length; i++) {
+function getFormData(formId) {
+    let f = document.getElementById(formId);
+    let out = {};
+    let e;
+    for (let i = 0; i < f.length; i++) {
         if (f[i].name != '' && f[i].disabled == false) {
             e = f[i].name.replaceAll("-", "_");
             switch (f[i].type.toLowerCase()) {
@@ -39,11 +39,10 @@ function get_form_data(form_id) {
 
 function createFormObjectByItemId(formName) {
     // Caution: out’s property value is string!!!
-    var out = new Object();
-    var frm = document.forms[formName];
+    let out = new Object();
+    let frm = document.forms[formName];
     for (i = 0; i < frm.length; i++) {
         out[frm[i].id] = frm[i].value;
     }
     return out;
 }
-
