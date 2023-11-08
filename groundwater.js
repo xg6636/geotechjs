af4b30d5ef221ed1f9cd9c078e3d3ead69d0b730
Jack Hsu <90829467+xg6636@users.noreply.github.com>
2023-11-08 18:11:12 +0800

    Add files via upload

---
+++ groundwater.js
@@ -0,0 +1,48 @@
+// groundwater functions
+// lib006
+// created at 2022/08/01 23:50:09
+// last modified at 2023-11-08 17:29:11
+// 
+// copyright (c) 2022 - 2023 Jack Hsu
+
+
+
+const GROUNDWATER = {
+    seepage: {
+        basises: ["JGJ120"],
+        jgj120: {
+            basis: "JGJ120-2012",
+            runFormulaC01(a) {
+                // param a: sample {d: 3, gamma: 18, hw: 5, gammaw: 9.8}
+                let kh = Number(a.d) * Number(a.gamma);
+                kh = kh / Number(a.hw) / Number(a.gammaw);
+                return kh;
+            },
+            runFormulaC02(a) {
+                // param a: sample {ld: 3, d1: 2, gamma1: 10, deltah: 5, gammaw: 9.8}
+                let kf = 2 * Number(a.ld) + 0.8 * Number(a.d1);
+                kf *= Number(a.gamma1);
+                kf = kf / Number(a.deltah) / Number(a.gammaw);
+                return kf;
+            }
+        }
+    },
+    tubeWell: {
+        basises: ["JGJ120-2012", "工程地质手册（第五版）"],
+        getMaxCapacity(a) {
+            // param a: sample {filter_radius: 0.3, filter_length: 18, k: 18}
+            let out = 120 * Math.PI;
+            out = out * Number(a.filter_radius) * Number(a.filter_length);
+            out = out * Math.pow(Number(a.k), 0.333333333);
+            return { basis: "JGJ120-2012", value: out };
+        },
+        getFilterLength(a) {
+            // param a: sample {well_capacity:1200, filter_diameter: 0.6, filter_ne: 0.4, k: 18}
+            let v = Math.sqrt(Number(a.k)) / 15;
+            let out = Number(a.well_capacity) / Math.PI;
+            out = out / Number(a.filter_diameter) / Number(a.filter_ne) / v;
+            return { basis: "工程地质手册（第五版）", value: out };
+        }
+    }
+
+};
