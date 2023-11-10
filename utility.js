a962faf84270228bc2abab138908a669a4103516
Jack Hsu <90829467+xg6636@users.noreply.github.com>
2023-11-10 15:54:46 +0800

    Add files via upload

--- utility.js
+++ utility.js
@@ -1,39 +1,27 @@
 // utilities
 // coded by Jack Hsu <jackhsu2010@gmail.com>
 // created at 2022/08/02 10:08:21f
-// last modified at 2023-11-10 15:28:05
+// last modified at 2023-11-10 15:51:12
 //
 // copyright (c) 2022 - 2023 Jack Hsu
 
+
+
 function getFormData(formId) {
-    let f = document.getElementById(formId);
-    let out = {};
-    let e;
-    let et;
-    for (let i = 0; i < f.length; i++) {
-        if (f[i].name != "" && f[i].disabled == false) {
-            e = f[i].name.replaceAll("-", "_");
-            et = f[i].type.toLowerCase();
-            switch (et) {
-                case "checkbox":
-                    if (out[e] == undefined) {
-                        out[e] = [];
-                    }
-                    if (f[i].checked) {
-                        out[e].push(f[i].value);
-                    }
-                    break;
-                case "radio":
-                    if (f[i].checked) {
-                        out[e] = f[i].value;
-                    }
-                    break;
-                default:
-                    out[e] = f[i].value;
-            }
+    const f = new FormData(document.getElementById(formId));
+    let d = {};
+    for (const [k, v] of f) {
+        let e = k.replaceAll("-", "_");
+        if (d[e] == undefined) {
+            d[e] = v;
+        } else if (Array.isArray(d[e])) {
+            d[e].push(v);
+        } else {
+            d[e] = [d[e]];
+            d[e].push(v);
         }
     }
-    return out;
+    return d;
 }
 
 function createFormObjectByItemId(formName) {
